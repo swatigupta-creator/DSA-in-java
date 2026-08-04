2f37c0783679ff390663ddaee7159a82f78d5330
swatigupta-creator <swatigupta98766@gmail.com>
2026-08-05 00:10:42 +0530

    Time: 4 ms (82.70%) | Memory: 46.3 MB (42.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,16 +1,4 @@
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         char[]arr1=s.toCharArray();
-//         char[]arr2=t.toCharArray();
-//     Arrays.sort(arr1);
-//     Arrays.sort(arr2);
-//     for(int i=0;i<arr1.length;i++){
-     
-// if(arr1[i]!=arr2[i]) return false; 
-//     }
-//  return true;
-//     }
-// }
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
